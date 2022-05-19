d30ceace13274a84d656dbe270345aeb05dd7bdb
3343780376 <3343780376@qq.com>
2022-05-19 18:57:19 +0800

    init the repo

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
+import {  Tabbar, TabbarItem } from "@nutui/nutui-react";
 import './App.css';
+import '@nutui/nutui-react/dist/style.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component<any, any>{
+
+  render() {
+    return <>
+      <Tabbar bottom>
+        <TabbarItem tabTitle="首页" href="" icon="home" />
+        <TabbarItem tabTitle="我的" href="######" icon="my" />
+      </Tabbar>
+    </>
+  }
 }
 
 export default App;
